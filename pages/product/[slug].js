7a485daa88e59d517e213d3f71c5f9a62be85172
Ons Hammouda <onsh73@gmail.com>
2022-10-16 22:28:51 +0100

    Video-08-Handle-Add-To-Cart

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,18 +1,29 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useContext } from 'react';
 
 import Layout from '../../components/Layout';
 import data from '../../utils/data';
+import { Store } from '../../utils/Store';
 
 export default function ProductScreen() {
+  const { state, dispatch } = useContext(Store);
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((x) => x.slug == slug);
   if (!product) {
     return <div>Product Not Found </div>;
   }
-
+  const addToCartHandler = () => {
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    if (product.countInStock < quantity) {
+      alert('Sorry. Product is out of stock');
+      return;
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+  };
   return (
     <Layout title={product.name}>
       <div className="py-2">
@@ -53,7 +64,12 @@ export default function ProductScreen() {
                 ${product.countInStock > 0 ? 'In stock' : 'Unavailable'}
               </div>
             </div>
-            <button className="primary-button w-full">Add to cart</button>
+            <button
+              className="primary-button w-full"
+              onClick={addToCartHandler}
+            >
+              Add to cart
+            </button>
           </div>
         </div>
       </div>
